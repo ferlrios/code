a5543e5d86b6fe078e4ea4b144443215ea436156
Ferlrios <rios.fernando1105@gmail.com>
2021-05-23 16:48:10 -0300

    Terminados los últimos detalles del desarrollo

--- R3/application.js
+++ R3/application.js
@@ -1,11 +1,14 @@
+/* Cuando carga la página se ejecuta lo de adentro */
 $(document).ready(function () {
+  /* Cuando se clickea el botón de play, se muestra el div modal que contiene el video */
   $("#play").on("click", function () {
     $("#modal-video").show();
   });
-
+  /* Cuando se clickea en la cruz del modal, vuelve a estar oculto */
   $(".btn-close").on("click", function () {
     $("#modal-video").hide();
   });
 });
 
+/* Por defecto, al modal lo ocultamos */
 $("#modal-video").hide();
--- R4/application.js
+++ R4/application.js
@@ -1,3 +1,4 @@
+/* Definí un array con ciudades a consultar a la API */
 let city = [
   "New_York",
   "Buenos _Aires",
@@ -5,16 +6,16 @@ let city = [
   "Paris",
   "Tokio",
   "Medellin",
-  "El Cairo",
+  "Toronto",
 ];
 
+/* Con un for recorremos el array pegandole a la API y trayendo a nuestra tabla (a través de la id de los <tr>) los datos. */
 for (let x = 0; x <= 6; x++) {
   let config = {
     type: "get",
     url: `http://api.weatherstack.com/current?access_key=64fd70580336a2dab8c706b0ffefdb22&query=${city[x]}`,
     dataType: "json",
     success: (response) => {
-      console.log(`Funciono ${response.location.name}`);
       $(`#row${x}>td:nth-child(1)`).html(`${response.location.name}`);
       $(`#row${x}>td:nth-child(2)`).html(`${response.location.country}`);
       $(`#row${x}>td:nth-child(3)`).html(`${response.location.region}`);
@@ -26,5 +27,6 @@ for (let x = 0; x <= 6; x++) {
     },
   };
 
+  /* En lugar de declarar el objeto dentro del parametro del método, lo hice antes en config y ahora lo llamo */
   $.ajax(config);
 }
